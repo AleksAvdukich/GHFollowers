eb3db084c00d2d6112041d98603d075fb58cd49e
Sanel Avdukich <avdukichsanel@mail.ru>
2020-01-06 23:39:12 +0300

    Setup UITabBarController & UINavigationController

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Aleksandr Avdukich on 06.01.2020.
+//  Copyright © 2020 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+        
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Aleksandr Avdukich on 06.01.2020.
+//  Copyright © 2020 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+        
+    }
+}
