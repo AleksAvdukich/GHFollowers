a996dad6ebd4a4f41f6d374cf597e5aa69e335cc
Sanel Avdukich <avdukichsanel@mail.ru>
2020-01-10 02:56:58 +0300

    Passing Data & Keyboards

---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,33 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Aleksandr Avdukich on 10.01.2020.
+//  Copyright © 2020 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
